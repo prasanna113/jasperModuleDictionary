46bfdee34776629f51cfdfb937cda286b2298b2b
prasanna113 <prasanna11deshmukh@gmail.com>
2015-12-31 00:12:51 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,27 @@
+import unirest
+import random
+import re
+
+WORDS = ["DEFINE", "DEFINITION"]
+
+def handle(text, mic, profile):
+    app_id = "oxiDR7gJ3AmshgH1jhbeo6vxBPctp1YtRXWjsnNEmuOH3WKBEl"
+    response = unirest.get("https://montanaflynn-dictionary.p.mashape.com/define?word=%s" % text, 
+                           headers={"X-Mashape-Key": app_id,
+                           "Accept": "application/json"}
+                           )
+    
+    body = response.body
+    messages = [body["definitions"][0]["text"],
+                body["definitions"][1]["text"]]
+    
+    message = random.choice(messages)
+    mic.say(message)
+    
+def isValid(text):
+    if re.search(r'\bdefine\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\bdefinition\b', text, re.IGNORECASE):
+        return True
+    else:
+        return False
